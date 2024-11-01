2458f6603322232c19a7342cb60be660ba23a0ae
Carla Leone <carlaleone@Carlas-MacBook-Pro.local>
2024-11-01 17:21:31 +0000

    trying

--- GIS/Assignment 1/assignment1.R
+++ GIS/Assignment 1/assignment1.R
@@ -1,7 +1,19 @@
 library(readr)
 joined <- read_csv("GIS/Assignment 1/joined_layer_1.csv")
 View(joined)
-plot(joined$dist_patrol~joined$n)
-plot(joined$n~joined$distance_river)
+plot(joined$n~joined$dist_patrol)
+plot(joined$n~joined$distance_road)
+joined$n<- as.numeric(joined$n)
+
+lm_road<-lm(n~distance_road, data=joined)
+summary(lm_road)
+plot(lm_road)
+
+
+
+plot(joined$n~joined$distance_road)
+abline(lm_road)
+
+
 joined$n<- as.numeric(joined$n)
 
