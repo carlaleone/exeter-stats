d391fe6c3bd336ec9f2a8083e9f728374ad0d1ed
Carla Leone <carlaleone@Carlas-MacBook-Pro.local>
2024-11-06 09:27:00 +0000

    adjust code

--- GIS/Assignment 1/water.R
+++ GIS/Assignment 1/water.R
@@ -4,4 +4,7 @@ getwd()
 water <- read_csv("/Users/carlaleone/Desktop/Exeter/GIS/Assignment 1/water_distances.csv")
 View(water)
 
-plot(water$n~water$distance_river)
+plot(water$density~water$distance_river)
+plot(water$density~water$distance_waterhole)
+
+lm
