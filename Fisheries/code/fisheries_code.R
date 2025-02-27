71f3fc439db59e0c6cfdbef5b74605eb8e3455a8
Carla Leone <carlaleone@Carlas-MacBook-Pro.local>
2025-02-27 14:46:03 +0000

    created word doc

--- Fisheries/code/fisheries_code.R
+++ Fisheries/code/fisheries_code.R
@@ -7,9 +7,42 @@ getwd()
 setwd("/Users/carlaleone/Desktop/Exeter/Fisheries")
 library(tidyverse)
 library(readxl)
-quantity <- read_excel("Fisheries.xlsx", 
+total_quantity <- read_excel("Fisheries.xlsx", 
                                     sheet = "Total Quantity")
-value<- read_excel("Fisheries.xlsx", 
+total_value<- read_excel("Fisheries.xlsx", 
                    sheet = "Total Quality")
+
+
+## Subset to the focal species ----
+quantity <- total_quantity %>%
+  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole")) %>%
+  select(-c(2:6))
+
+value <- total_value %>%
+  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole"))%>%
+  select(-c(2:6))
+
 View(quantity)
 View(value)
+
+
+
+## Initial plots----
+#first change to long format
+quantity_long <- quantity %>%
+  pivot_longer(cols = -Species, names_to = "Year", values_to = "n")
+
+View(quantity_long)
+
+# Convert Year to numeric (if necessary)
+quantity_long$Year <- as.numeric(quantity_long$Year)
+
+# now plot
+ggplot(data = quantity_long, aes(x = Year, y = n, color = Species, group = Species)) +
+  geom_line(linewidth = 1) + 
+  geom_point() +
+  labs(title = "Species Trends Over the Years",
+       x = "Year",
+       y = "Quantity ('000 tonnes)",
+       color = "Species") +
+  theme_minimal()
