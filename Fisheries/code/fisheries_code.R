2a76ae4fbb31d841395f796471ae517ad3838e93
Carla Leone <carlaleone@Carlas-MacBook-Pro.local>
2025-03-07 10:10:45 +0000

    finished graphs for question 1 fisheries assignment

--- Fisheries/code/fisheries_code.R
+++ Fisheries/code/fisheries_code.R
@@ -27,7 +27,7 @@ View(value)
 
 
 
-## Initial plots----
+## Initial plots Quantity----
 #first change to long format
 quantity_long <- quantity %>%
   pivot_longer(cols = -Species, names_to = "Year", values_to = "n")
@@ -41,8 +41,35 @@ quantity_long$Year <- as.numeric(quantity_long$Year)
 ggplot(data = quantity_long, aes(x = Year, y = n, color = Species, group = Species)) +
   geom_line(linewidth = 1) + 
   geom_point() +
-  labs(title = "Species Trends Over the Years",
-       x = "Year",
+  scale_colour_manual(values=cbbPalette)+
+  labs(x = "Year",
        y = "Quantity ('000 tonnes)",
        color = "Species") +
-  theme_minimal()
+  theme_classic() +
+  scale_y_continuous(breaks = seq(0, max(quantity_long$n, na.rm = TRUE), by = 20))+
+  scale_x_continuous(breaks = seq(0, max(quantity_long$Year, na.rm = TRUE), by = 1))
+
+
+## Initial plots value ----
+value_long <- value %>%
+  pivot_longer(cols = -Species, names_to = "Year", values_to = "n")
+
+View(value_long)
+
+# Convert Year to numeric (if necessary)
+value_long$Year <- as.numeric(value_long$Year)
+
+#colour palatte colour blind freindly
+cbbPalette <- c("#009E73", "#F0E442",  "#D55E00", "#CC79A7", "#0072B2")
+
+# now plot
+ggplot(data = value_long, aes(x = Year, y = n, color = Species, group = Species)) +
+  geom_line(linewidth = 1) + 
+  geom_point() +
+  scale_colour_manual(values=cbbPalette)+
+  labs(x = "Year",
+       y = "Value (£ million)",
+       color = "Species") +
+  theme_classic() +
+  scale_y_continuous(breaks = seq(0, max(value_long$n, na.rm = TRUE), by = 20))+
+  scale_x_continuous(breaks = seq(0, max(value_long$Year, na.rm = TRUE), by = 1))
