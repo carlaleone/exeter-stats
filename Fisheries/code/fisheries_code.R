ce63e93275c5f3326bc7f1ce96a43d44d526c333
Carla Leone <carlaleone@Carlas-MacBook-Pro.local>
2025-03-22 15:32:47 +0000

    adjusting some graphs x axis

--- Fisheries/code/fisheries_code.R
+++ Fisheries/code/fisheries_code.R
@@ -14,15 +14,18 @@ total_value<- read_excel("Fisheries.xlsx",
 scotland <- read_excel("Fisheries.xlsx", 
                        sheet = "scottish fleet")
 
+value_and_catch_total <- read_excel("Fisheries.xlsx", 
+                       sheet = "value and catch total uk") 
 
 ## Subset to the focal species ----
 quantity <- total_quantity %>%
-  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole")) %>%
+  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole", "Total All Species")) %>%
   select(-c(2:6))
 
 value <- total_value %>%
   filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole"))%>%
   select(-c(2:6))
+
 
 View(quantity)
 View(value)
@@ -76,6 +79,38 @@ ggplot(data = value_long, aes(x = Year, y = n, color = Species, group = Species)
   scale_y_continuous(breaks = seq(0, max(value_long$n, na.rm = TRUE), by = 20))+
   scale_x_continuous(breaks = seq(0, max(value_long$Year, na.rm = TRUE), by = 1))
 
+## Total catch and value UK ----
+value_and_catch_total_long <- value_and_catch_total %>%
+  pivot_longer(cols = -n, names_to = "Year", values_to = "Value") %>%
+  rename(type = n)  %>%
+  mutate(type = case_when(
+    type == "value" ~ "Value (million £)",
+    type == "quantity" ~ "Catch (tonnes)"
+  ))
+
+value_and_catch_total_long$Year<- as.numeric(value_and_catch_total_long$Year)
+
+# now plot
+ggplot(data = value_and_catch_total_long, aes(x = Year, y = Value, color = type, group = type)) +
+  geom_line(linewidth = 1) + 
+  geom_point() +
+  scale_colour_manual(values=cbbPalette)+
+  labs(x = "Year",
+       y = "Quantity ('000 tonnes)",
+       color = "Species") +
+  theme_classic() +
+  scale_y_continuous(breaks = seq(0, max(quantity_long$n, na.rm = TRUE), by = 20))+
+  scale_x_continuous(breaks = seq(0, max(quantity_long$Year, na.rm = TRUE), by = 1))
+
+
+ggplot(value_and_catch_total_long, aes(x = factor(Year), y = Value, fill = type)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8)) +  # Separated bars
+  scale_fill_manual(values = c("steelblue", "darkorange")) +  # Custom colors
+  scale_x_discrete(expand = expansion(mult = c(0.1, 0.1))) +  # Ensures proper spacing
+  labs( x = "Year", y = "Value ('000)", fill = "Value Type") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 ## Scottish Fleet Size ----
 scotland_fleet <- read_excel("Fisheries.xlsx", 
                        sheet = "agg scottish fleet") 
@@ -118,14 +153,16 @@ ggplot(data = scotland_long, aes(x = year, y = n, color = type, group = type)) +
   labs(x = "Year",
        y = "Value ('000)",
        color = "Value Type") +
-  theme_classic()
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, max(scotland_long$year, na.rm = TRUE), by = 1))
 
 
 ## Plot value/catch ----
 total_value_catch <- read_excel("Fisheries.xlsx", 
-                             sheet = "Value.Catch") 
+                             sheet = "Value.Catch")
+
 value_catch <- total_value_catch %>%
-  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole"))%>%
+  filter(Species %in% c("Cuttlefish","Lobsters", "Mackerel", "Cod", "Sole", "Total All Species"))%>%
   select(-c(2:6))
 
 value_catch_long <- value_catch %>%
@@ -149,3 +186,25 @@ ggplot(data = value_catch_long, aes(x = Year, y = n, color = Species, group = Sp
   scale_y_continuous(breaks = seq(0, max(value_long$n, na.rm = TRUE), by = 5))+
   scale_x_continuous(breaks = seq(0, max(value_long$Year, na.rm = TRUE), by = 1))
 
+
+## Value per catch in scottish fleet----
+
+ggplot(scotland, aes(x = factor(year), y = `value/quantity`)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  labs( x = "Year", y = "Value per ton caught (million £/ton )") +
+  theme_bw()
+
+## Scotland Effort ----
+scotland$small.big.boat.ration<- scotland$under10_n/scotland$over10_n
+scotland$small.big.gw.ratio<- scotland$under10_GT/scotland$over10_GT
+
+plot(scotland$small.big.boat.ration~scotland$year)
+View(scotland)
+
+ggploscotlandggplot(scotland, aes(x = factor(Year), y = Value, fill = type)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8)) +  # Separated bars
+  scale_fill_manual(values = c("steelblue", "darkorange")) +  # Custom colors
+  scale_x_discrete(expand = expansion(mult = c(0.1, 0.1))) +  # Ensures proper spacing
+  labs( x = "Year", y = "Value ('000)", fill = "Value Type") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
